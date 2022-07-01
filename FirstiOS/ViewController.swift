f16c8e5fde6ac8795f5a7b6e559aabc7505a04f8
sunminhong <akoda5555@gmail.com>
2022-07-01 17:11:40 +0900

    Initial Commit

---
+++ FirstiOS/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  FirstiOS
+//
+//  Created by SunMin Hong on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 제목
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "헬로우월드!!!"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        view.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+
+}
+
